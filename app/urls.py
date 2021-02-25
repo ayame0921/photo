4f6b4cd398ea80f99fd674f5d414bc61b0757f06
shin-carpediem <buru.aoshin@gmail.com>
2021-02-25 09:43:19 +0900

    fix indent

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('users/<int:pk>', views.users_detail, name='users_detail'),
+    path('users/<int:pk>/', views.users_detail, name='users_detail'),
     path('photos/new/', views.photos_new, name='photos_new'),
     path('photos/<int:pk>/', views.photos_detail, name='photos_detail'),
     path('photos/<int:pk>/delete/',views.photos_delete, name='photos_delete'),
